67d630964233b9e49d6b0c3b6bed33a727035330
majiajun <864428661@qq.com>
2022-01-07 23:51:42 -0800

    mission management interface modification Signed-off-by: majiajun <864428661@qq.com>
    Change-Id: I7b498018a0fd504b0adde6945996777a7b154823

--- services/bundlemgr/test/mock/include/mock_ability_mgr_host.h
+++ services/bundlemgr/test/mock/include/mock_ability_mgr_host.h
@@ -320,6 +320,14 @@ public:
     {
         return 0;
     }
+    int StartSyncRemoteMissions(const std::string& devId, bool fixConflict, int64_t tag) override
+    {
+        return 0;
+    }
+    int StopSyncRemoteMissions(const std::string& devId) override
+    {
+        return 0;
+    }
 };
 }  // namespace AppExecFwk
 }  // namespace OHOS
