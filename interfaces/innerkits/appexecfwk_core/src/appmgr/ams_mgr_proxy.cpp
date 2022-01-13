1077f16505fb6f001e6f6285c66ca9d213e02b42
gwang <wanggenliang@huawei.com>
2022-01-13 03:17:46 +0000

    回退 'Pull Request !317 : 回退 'Pull Request !310 : 新增启动&切换多用户功能''

--- interfaces/innerkits/appexecfwk_core/src/appmgr/ams_mgr_proxy.cpp
+++ interfaces/innerkits/appexecfwk_core/src/appmgr/ams_mgr_proxy.cpp
@@ -36,7 +36,8 @@ bool AmsMgrProxy::WriteInterfaceToken(MessageParcel &data)
 }
 
 void AmsMgrProxy::LoadAbility(const sptr<IRemoteObject> &token, const sptr<IRemoteObject> &preToken,
-    const std::shared_ptr<AbilityInfo> &abilityInfo, const std::shared_ptr<ApplicationInfo> &appInfo)
+    const std::shared_ptr<AbilityInfo> &abilityInfo, const std::shared_ptr<ApplicationInfo> &appInfo,
+    int32_t uid)
 {
     APP_LOGD("start");
     if (!abilityInfo || !appInfo) {
@@ -54,6 +55,7 @@ void AmsMgrProxy::LoadAbility(const sptr<IRemoteObject> &token, const sptr<IRemo
     data.WriteParcelable(preToken);
     data.WriteParcelable(abilityInfo.get());
     data.WriteParcelable(appInfo.get());
+    data.WriteInt32(uid);
     sptr<IRemoteObject> remote = Remote();
     if (remote == nullptr) {
         APP_LOGE("Remote() is NULL");
