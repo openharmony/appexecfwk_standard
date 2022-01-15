5ad5bd2c1db3862c1d862abb7a9c9b5a0405322b
wanghang <408861745@qq.com>
2022-01-15 11:44:59 +0800

    add d-bms
    Signed-off-by: wanghang <408861745@qq.com>
    Change-Id: I788ee1217fe24d0ad15ad9ffa3fbc2a97eaf9bf2

---
+++ services/dbms/include/distributed_bms_interface.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FOUNDATION_APPEXECFWK_SERVICES_DBMS_INCLUDE_DISTRIBUTED_BMS_INTERFACE_H
+#define FOUNDATION_APPEXECFWK_SERVICES_DBMS_INCLUDE_DISTRIBUTED_BMS_INTERFACE_H
+
+#include <string>
+#include <vector>
+
+#include "element_name.h"
+#include "iremote_broker.h"
+#include "remote_ability_info.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+class IDistributedBms : public IRemoteBroker {
+public:
+    DECLARE_INTERFACE_DESCRIPTOR(u"ohos.appexecfwk.IDistributedbms");
+
+    /**
+     * @brief get remote ability info
+     * @param elementName Indicates the elementName.
+     * @param remoteAbilityInfo Indicates the remote ability info.
+     * @return Returns true when get remote ability info success; returns false otherwise.
+     */
+    virtual bool GetRemoteAbilityInfo(
+        const OHOS::AppExecFwk::ElementName &elementName, RemoteAbilityInfo &remoteAbilityInfo) = 0;
+
+    enum class Message {
+        GET_REMOTE_ABILITY_INFO = 1,
+    };
+};
+}  // namespace AppExecFwk
+}  // namespace OHOS
+#endif  // FOUNDATION_APPEXECFWK_SERVICES_DBMS_INCLUDE_DISTRIBUTED_BMS_INTERFACE_H
---
+++ services/dbms/src/distributed_bms_proxy.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "distributed_bms_proxy.h"
+
+#include "hilog_wrapper.h"
+#include "parcel_macro.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+DistributedBmsProxy::DistributedBmsProxy(const sptr<IRemoteObject> &object) : IRemoteProxy<IDistributedBms>(object)
+{
+    HILOG_INFO("DistributedBmsProxy instance is created");
+}
+
+DistributedBmsProxy::~DistributedBmsProxy()
+{
+    HILOG_INFO("DistributedBmsProxy instance is destroyed");
+}
+
+bool DistributedBmsProxy::GetRemoteAbilityInfo(
+    const OHOS::AppExecFwk::ElementName &elementName, RemoteAbilityInfo &remoteAbilityInfo)
+{
+    APP_LOGD("DistributedBmsProxy GetRemoteAbilityInfo");
+    MessageParcel data;
+    MessageParcel reply;
+    if (!data.WriteParcelable(&elementName)) {
+        HILOG_ERROR("DistributedBmsProxy GetRemoteAbilityInfo write elementName error");
+        return false;
+    }
+    if (!SendRequest(IDistributedBms::Message::GET_REMOTE_ABILITY_INFO, data, reply)) {
+        return false;
+    }
+    if (!GetParcelableInfo<RemoteAbilityInfo>(
+            IDistributedBms::Message::GET_REMOTE_ABILITY_INFO, data, remoteAbilityInfo)) {
+        APP_LOGE("fail to query ability info mutiparam from server");
+        return false;
+    }
+    return true;
+}
+
+bool DistributedBmsProxy::SendRequest(IDistributedBms::Message code, MessageParcel &data, MessageParcel &reply)
+{
+    APP_LOGD("DistributedBmsProxy SendRequest");
+    sptr<IRemoteObject> remote = Remote();
+    MessageOption option(MessageOption::TF_SYNC);
+    if (remote == nullptr) {
+        HILOG_ERROR("fail to send %{public}d cmd to service due to remote object is null", code);
+        return false;
+    }
+    int32_t result = remote->SendRequest(static_cast<uint32_t>(code), data, reply, option);
+    if (result != OHOS::NO_ERROR) {
+        HILOG_ERROR("fail to send %{public}d cmd to service due to transact error", code);
+        return false;
+    }
+    return true;
+}
+
+template<typename T>
+bool DistributedBmsProxy::GetParcelableInfo(IDistributedBms::Message code, MessageParcel &data, T &parcelableInfo)
+{
+    MessageParcel reply;
+    if (!SendRequest(code, data, reply)) {
+        return false;
+    }
+
+    if (!reply.ReadBool()) {
+        APP_LOGE("reply result false");
+        return false;
+    }
+
+    std::unique_ptr<T> info(reply.ReadParcelable<T>());
+    if (!info) {
+        APP_LOGE("readParcelableInfo failed");
+        return false;
+    }
+    parcelableInfo = *info;
+    APP_LOGD("get parcelable info success");
+    return true;
+}
+}  // namespace AppExecFwk
+}  // namespace OHOS
