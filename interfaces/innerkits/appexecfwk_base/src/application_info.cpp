c5def2e9fe54441af45956a1bc4f292827f294db
SoftSquirrel <xuhao47@huawei.com>
2022-02-16 21:04:31 +0800

    IssueNo: #I4PKYS Description: codedex Feature or Bugfix: Bugfix Binary Source: No
    Signed-off-by: SoftSquirrel <xuhao47@huawei.com>

--- interfaces/innerkits/appexecfwk_base/src/application_info.cpp
+++ interfaces/innerkits/appexecfwk_base/src/application_info.cpp
@@ -233,7 +233,7 @@ void ApplicationInfo::Dump(std::string prefix, int fd)
     }
     int flags = fcntl(fd, F_GETFL);
     if (flags < 0) {
-        APP_LOGE("dump ApplicationInfo fcntl error %{public}s", strerror(errno));
+        APP_LOGE("dump ApplicationInfo fcntl error, errno : %{public}d", errno);
         return;
     }
     uint uflags = static_cast<uint>(flags);
@@ -245,7 +245,7 @@ void ApplicationInfo::Dump(std::string prefix, int fd)
         result.append(jsonObject.dump(Constants::DUMP_INDENT));
         int ret = TEMP_FAILURE_RETRY(write(fd, result.c_str(), result.size()));
         if (ret < 0) {
-            APP_LOGE("dump ApplicationInfo write error %{public}s", strerror(errno));
+            APP_LOGE("dump ApplicationInfo write error, errno : %{public}d", errno);
         }
     }
     return;
